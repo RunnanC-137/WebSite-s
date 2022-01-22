07ff974bc41914879bfce80a96191aaaf83b1597
RunnanC-137 <ruanlons@gmail.com>
2022-01-22 14:46:31 -0300

    as fotos

--- the site/script.js
+++ the site/script.js
@@ -7,11 +7,11 @@ function carregar() {
     msg.innerHTML = `Agora são ${hora}:${minutos} horas`
     if (hora >= 0 && hora < 12) {
         document.body.style.background = '#dc8e4d'
-        foto.src = '../Fotos/manha.png'
+        foto.src = 'Fotos/manha.png'
     } else if (hora >= 12 && hora < 18) {
         document.body.style.background = '#7b4e75'
     } else {
-        foto.src = '../Fotos/noite.png'
+        foto.src = 'Fotos/noite.png'
         document.body.style.background = '#00192d'
     }
 
